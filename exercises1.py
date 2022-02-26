87c8c2eb70e24e87815c057410aa28ec4cf6d41a
Roberto Munoz <Rjmunoz24@gmail.com>
2022-02-25 21:50:24 -0600

    CAC-103 My First Application

---
+++ exercises1.py
@@ -0,0 +1,45 @@
+# exercises1
+# can you make a typo that generates an error?
+# the error I made was pint hello world before fixing it
+print("hello world")
+
+# can you make sense of the error message
+# yes the error makes sense to me when I figured it out
+# this is the error I got
+# 'pint' is not defined
+
+# can you make a typo that doesn't generate an error?
+message = "hello world"
+print(message)
+# why didn't it make an error?
+# because the variables were spelled the same
+
+
+# page 19 2-1 2-2
+mail = "address of person"
+print(mail)
+
+mail = "Robert Munoz"
+print(mail)
+# pretty much what I was doing is putting a message to a variable and print
+# then assigned a variable to a new message and print the new
+
+
+# page 131 8-1 8-2
+def display_message():
+    """Display a simple sentence"""
+    message = "I'm learning to store code in functions"
+    print(message)
+
+display_message()
+
+def favorite_book(title):
+    """Display a message about someone's favorite book"""
+    print("Green Eggs And Ham + is my favorite book")
+
+favorite_book("Green Eggs And Ham")
+
+
+
+
+
