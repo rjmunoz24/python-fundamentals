c4bf15b1d49456f5540fd6ae36ba4a6f86366702
Roberto Munoz <Rjmunoz24@gmail.com>
2022-03-02 16:43:18 -0600

    CAC 102-2 My Application Overview Exercises 2

---
+++ exercises/exercises2.py
@@ -0,0 +1,77 @@
+# My Application Overview
+
+# Write a function called simple().
+# Assign a different message to 5 variables
+# And print each message.
+def simple():
+    """Months Of The Year"""
+    message = 'january'
+    print("message")
+    message = 'february'
+    print("message")
+    message = 'march'
+    print("message")
+    message = 'april'
+    print("message")
+    message = 'may'
+    print("message")
+
+
+simple()
+
+# write a function called simple2().
+# assign a message to a variable, then print out the variable.
+# change the message and assign it to the variable again, but after the first print statement.
+# print the second message.
+# do these steps 2 more times.
+# you should have 4 messages assigned to the same variable and 4 print functions showing the results
+def simple2():
+    """These are the first of the months"""
+    able = "January is the first month"
+    print(able)
+    able = "February is the second month"
+    print(able)
+    able = "March is the third month"
+    print(able)
+    able = "April is the forth month"
+    print(able)
+
+
+simple2()
+
+
+# write a function called message that takes 1 argument.
+# inside that function, write a print function that takes the argument.
+def message(arg1):
+    print(arg1)
+
+
+message('holiday')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
