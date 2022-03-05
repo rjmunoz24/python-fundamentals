c8e56a240da37884174ec8d315e972cf0785624f
Roberto Munoz <Rjmunoz24@gmail.com>
2022-03-04 23:17:58 -0600

    CAC 102-3 exercises All about strings

---
+++ exercises/exercises3.py
@@ -0,0 +1,32 @@
+# 2-3
+name = 'Roberto'
+print(f"Hello, {name}! How are you today!")
+
+# 2-4
+other_name = 'Munoz'
+print(other_name.lower())
+print(other_name.upper())
+print(other_name.title())
+
+# 2-5
+print("Kobe Bryant once said,\"The moment you give up, is the moment you let someone else win")
+
+# 2-6
+famous_person = 'Kobe bryant'
+message = 'The moment you give up, is the moment you let someone else win'
+print(f"{famous_person} once said, \"{message}\"")
+
+# 2-7
+name = '\t\nRoberto Munoz\t'
+print(name)
+print(f"{name} hello guys what to do today.")
+
+
+
+
+
+
+
+
+
+
