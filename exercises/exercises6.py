a4dff45fc55631182add1bf9de7ba59a0df4a32a
Roberto Munoz <Rjmunoz24@gmail.com>
2022-03-07 14:11:05 -0600

    CAC 102-6 Exercise Control Flow

---
+++ exercises/exercises6.py
@@ -0,0 +1,103 @@
+# on page 84 of your book, do 5-3 of the Try It Yourself
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You earned 5 points")
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You earned 5 points")
+
+
+# on page 84 of your book, do 5-4 of the Try It Yourself
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("5 points for shooting the alien")
+else:
+    print("You earned 10 points.")
+
+# on page 84 of your book, do 5-5 of the Try it Yourself
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You earned 5 points.")
+elif alien_color == 'yellow':
+    print("You earned 10 points.")
+elif alien_color == 'red':
+    print("You earned 15 points.")
+
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You earned 5 points.")
+elif alien_color == 'yellow':
+    print("You earned 10 points.")
+elif alien_color == 'red':
+    print("You earned 15 points.")
+
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You earned 5 points.")
+elif alien_color == 'yellow':
+    print("You earned 10 points.")
+elif alien_color == 'red':
+    print("You earned 15 points.")
+
+
+# On page 84 of your book, do 5-6 of the Try it yourself
+age = 1
+if age < 2:
+    print("The person is a baby")
+elif age < 4:
+    print("The person is a toddler")
+elif age < 13:
+    print("The person is a kid")
+elif age < 20:
+    print("The person is a teenager")
+elif age < 65:
+    print("The person is a adult")
+elif age >= 65:
+    print("The person is an elder")
+
+# write a function that takes an argument.
+# Check this argument to see if it is Boolean using the bool method.
+# call the method and use the below values as your argument
+# using comments, provide the name of the argument and if it was true or false from running the code.
+a = 3
+b = 1
+if a > b:
+    print("a is greater than b")
+if True:
+    print("a is greater than b")
+boolean_value = a > b
+print(boolean_value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
