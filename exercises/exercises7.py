2624ef3c68bd4462964735295817491722aa6298
Roberto Munoz <Rjmunoz24@gmail.com>
2022-03-08 00:30:01 -0600

    CAC 102-7 Exercises looping

---
+++ exercises/exercises7.py
@@ -0,0 +1,26 @@
+# on page 60 of your book, do 4-3 of the try it yourself
+# 4-3
+def basic_four():
+    for charlie in range(1, 21):
+        print(charlie)
+
+
+# basic_four()
+
+# 4-7
+threes = list(range(3, 31, 3))
+for number in threes:
+    print(number)
+
+
+# 4-8
+cubes = []
+for number in range(1, 11):
+    cube = number**3
+    cubes.append(cube)
+for cube in cubes:
+    print(cube)
+
+
+
+
